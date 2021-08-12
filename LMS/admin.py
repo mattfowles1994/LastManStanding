5cd616679bd82bb00157c1d9220fef4813e00086
Matt Fowles <matafowles@googlemail.com>
2021-08-12 21:25:19 +0100

    Add basic gameweek migrations

--- LMS/admin.py
+++ LMS/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Gameweek, Choice
+from .models import Gameweek, Choice, Team
 
 # Register your models here.
 class GameweekAdmin(admin.ModelAdmin):
@@ -8,6 +8,10 @@ class GameweekAdmin(admin.ModelAdmin):
 class ChoicesAdmin(admin.ModelAdmin):
         list_display = ('user_id', 'gameweek_id')
 
+class TeamsAdmin(admin.ModelAdmin):
+        list_display = ('short_name', 'long_name')
+
 
 admin.site.register(Gameweek, GameweekAdmin)
 admin.site.register(Choice, ChoicesAdmin)
+admin.site.register(Team, TeamsAdmin)
--- LMS/models.py
+++ LMS/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    fpl_id = models.SmallIntegerField(primary_key=True)
+    short_name = models.CharField(max_length=20)
+    long_name = models.CharField(max_length=30)
+
 class Gameweek(models.Model):
-    home_team = models.CharField(max_length=20)
-    away_team = models.CharField(max_length=20)
+    gameweek_id = models.SmallIntegerField(default=0)
+    home_team = models.ForeignKey("team", related_name='home_team', on_delete=models.CASCADE)
+    away_team = models.ForeignKey("team", related_name='away_team', on_delete=models.CASCADE)
     result = models.CharField(max_length=1, blank=True, null=True)
     score = models.CharField(max_length=5, blank=True, null=True)
     date= models.DateField(auto_now=False, auto_now_add=False)
     time = models.TimeField(auto_now=False, auto_now_add=False)
+    finished = models.BooleanField(default=False)
+
+
 
 class Choice(models.Model):
     user_id = models.IntegerField()
